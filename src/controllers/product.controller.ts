b184d664b0165e9380c0d8d597741f7c536f4e49
Zeyad <zeyadw526@gmail.com>
2025-05-09 17:42:16 +0300

    feat: add catchAsync utility for async error handling in Express

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,38 +1,30 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import Product from '../models/product.model';
+import catchAsync from '../utils/catchAsync';
 
-export const getAllProducts = async (req: Request, res: Response) => {
- try {
-  const products = await Product.find();
+export const getAllProducts = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const products = await Product.find();
 
-  res.status(200).json({
-   status: 'success',
-   results: products.length,
-   data: {
-    products,
-   },
-  });
- } catch (err) {
-  res.status(404).json({
-   status: 'fail',
-   message: err,
-  });
- }
-};
+    res.status(200).json({
+      status: 'success',
+      results: products.length,
+      data: {
+        products,
+      },
+    });
+  }
+);
 
-export const createProduct = async (req: Request, res: Response) => {
- try {
-  const newProduct = await Product.create(req.body);
-  res.status(201).json({
-   status: 'success',
-   data: {
-    tour: newProduct,
-   },
-  });
- } catch (err) {
-  res.status(400).json({
-   status: 'fail',
-   message: err,
-  });
- }
-};
+export const createProduct = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const newProduct = await Product.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newProduct,
+      },
+    });
+  }
+);
