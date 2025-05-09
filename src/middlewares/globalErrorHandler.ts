e9e7a024d9b599632a3df1d17467bc770c9c5f19
Zeyad <zeyadw526@gmail.com>
2025-05-09 18:45:42 +0300

    feat: distinguish between errors in development and production

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,12 +1,39 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, response } from 'express';
 import APIError from '../utils/APIError';
+import env from '../config/env';
+import logger from '../config/logger';
+
+const sendErrorDev = (err: any, res: Response): void => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
+};
+
+const sendErrorProd = (err: any, res: Response): void => {
+  // Operational, trusted error -> send message to the client.
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+
+  // Programming or unknow error -> don't leak error details.
+  logger.error('ERROR: ', err);
+
+  res.status(500).json({
+    status: 'error',
+    message: 'something went wrong',
+  });
+};
 
 export default (err: any, req: Request, res: Response, next: NextFunction) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (env.NODE_ENV === 'development') sendErrorDev(err, res);
+  else if (env.NODE_ENV === 'production') sendErrorProd(err, res);
 };
