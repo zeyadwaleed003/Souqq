0020e835330b026d900cdcc3b2e527abdf88e997
Zeyad <zeyadw526@gmail.com>
2025-05-11 11:43:09 +0300

    refactor: place the jwt stuff away from the controllers

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,23 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 
 import catchAsync from '../utils/catchAsync';
 import AuthService from '../services/auth.service';
-import env from '../config/env';
 import APIError from '../utils/APIError';
+import { generateToken } from '../utils/token';
 
 export const signup = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const newUser = await AuthService.create(req.body);
 
-    const token = jwt.sign(
-      { id: newUser.id },
-      env.JWT_SECRET as string,
-      {
-        expiresIn: `${env.JWT_EXPIRES_IN}d`,
-      } as object
-    );
+    const token = generateToken(newUser.id);
 
     res.status(201).json({
       status: 'success',
@@ -41,13 +34,7 @@ export const login = catchAsync(
     const isUser = await bcrypt.compare(password, user.password as string);
     if (!isUser) return next(new APIError('Invalid email or password', 404));
 
-    const token = jwt.sign(
-      { id: user.id },
-      env.JWT_SECRET as string,
-      {
-        expiresIn: `${env.JWT_EXPIRES_IN}d`,
-      } as object
-    );
+    const token = generateToken(user.id);
 
     res.status(201).json({
       status: 'success',
---
+++ src/utils/token.ts
@@ -0,0 +1,12 @@
+import jwt from 'jsonwebtoken';
+import env from '../config/env';
+
+export const generateToken = (id: string): string => {
+  return jwt.sign(
+    { id },
+    env.JWT_SECRET as string,
+    {
+      expiresIn: `${env.JWT_EXPIRES_IN}d`,
+    } as object
+  );
+};
